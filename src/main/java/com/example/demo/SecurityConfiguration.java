83574efb9c83043cdeca14f6ef8a87643a6b9f44
GBTC441006UR <temitopem@gmail.com>
2019-07-16 16:11:30 -0400

    ommmmmmmmmmmmmmmmmmmmmmiiiiiiia

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,31 +19,42 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration extends  WebSecurityConfigurerAdapter
 {
     @Bean
-    public static BCryptPasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Autowired
+    private SSUserDetailsService userDetailsService;
+    @Autowired
+    private userRepository appUserRepository;
+    @Override
+    public UserDetailsService 0userDetailsServiceBean() throws
+            Exception {
+        return new SSUserDetailsService(appUserRepository);
+    }
+
     @Override
     protected void configure (HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/")
-                .access("hasAnyAuthority('USER','ADMIN')")
+        http
+                .authorizeRequests()
+                .antMatchers("/","h2-console/**").permitAll()
+                .antMatchers("/").access("hasAnyAuthority('USER','ADMIN')")
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .logout()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout()
                 .logoutRequestMatcher(
                         new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").permitAll();
+                .logoutSuccessUrl("/login").permitAll().permitAll()
+                .and()
+                .httpBasic();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
-    @Override 
+    @Override
     protected void configure (AuthenticationManagerBuilder auth )
     throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("dave").password(passwordEncoder().encode("begreat")).authorities("ADMIN")
+        auth.userDetailsService(userDetailsServiceBean())
 
-                .and()
-                .withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
+                .passwordEncoder(passwordEncoder());
     }
 }
